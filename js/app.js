c111570b419b963be74978dc72a4b3441eb0be30
germassen <massengale.g@gmail.com>
2021-01-31 15:41:36 -0800

    Carousels Working
    YAYYYYY

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $('#recipeCarousel').carousel({
   })
   
   $('.carousel .carousel-item').each(function(){
-      var minPerSlide = 3;
+      var minPerSlide = 4;
       var next = $(this).next();
       if (!next.length) {
       next = $(this).siblings(':first');
@@ -22,4 +22,30 @@ $('#recipeCarousel').carousel({
 
   function hideIcon(self) {
     self.style.backgroundImage = 'none';
+}
+
+$('#restoCarousel').carousel({
+  interval: 10000
+})
+
+$('.carousel .carousel-item').each(function(){
+    var minPerSlide = 4;
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    for (var i=0;i<minPerSlide;i++) {
+        next=next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+          }
+        
+        next.children(':first-child').clone().appendTo($(this));
+      }
+});
+
+function hideIcon(self) {
+  self.style.backgroundImage = 'none';
 }
